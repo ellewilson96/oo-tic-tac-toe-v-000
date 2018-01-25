aa72e6b7d61f89f3683bc86788f5fa8f817a6fe8
Elle Wilson <eleanorwilsonn@gmail.com>
2018-01-25 18:07:01 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -82,7 +82,7 @@ end
 
 def won?
 WIN_COMBINATIONS.find do |win_combo|
-  board[win_combo[0]] == board[win_combo[1]] && board[win_combo[0]] == board[win_combo[2]] && position_taken?(board, win_combo[1])
+  @board[win_combo[0]] == @board[win_combo[1]] && @board[win_combo[0]] == @board[win_combo[2]] && position_taken?(win_combo[1])
 end
 end
 
