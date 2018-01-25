eb3cefaf9bdaefb942f88044d0a5c889351b2107
Elle Wilson <eleanorwilsonn@gmail.com>
2018-01-25 17:49:52 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,24 +11,9 @@ class TicTacToe
       @board.count{|token| token == "X" || token == "O"}
     end
 
-    def WIN_COMBINATIONS
-    end
+    WIN_COMBINATIONS = TicTacToe.new
+    WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
-
-    def move
-    end
-    def position_taken?
-    end
-    def valid_move?
-    end
-    def turn
-      def input_to_index
-        def valid_move?
-          def current_player
-      end
-    end
-  end
-end
     def display_board
       puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
       puts "-----------"
